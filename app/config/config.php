07c1a3f6b943c55f5e2dcb15d21d5706655248fd
IqroNegoro <iqronegoro0@gmail.com>
2022-02-28 09:29:36 +0700

    add .htacces

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php 
+
+define('BASEURL',"http://localhost/phpmvc/public");
+// DB
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','mahasiswakirara'); //ubah aja ni yg di db mu, w buat db baru soalnya phpmvc udah ada takutnya nabrak
