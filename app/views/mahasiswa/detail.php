b9dfdff38d326d6665f2b77e3e15ffff5ca3e985
Kirara02 <fathul.sirnaraja92@gmail.com>
2022-03-01 07:45:49 +0700

    mengubah view

--- app/views/mahasiswa/detail.php
+++ app/views/mahasiswa/detail.php
@@ -1,10 +1,10 @@
 <div class="container mt-5">
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title"><?= $data['mhs']['nama']?></h5>
-    <h6 class="card-subtitle mb-2 text-muted"<?= $data['mhs']['anime']?></h6>
-    <p class="card-text"><?= $data['mhs']['umur']?></p>
-    <p class="card-text"><?= $data['mhs']['status']?></p>
+    <h5 class="card-title">Nama : <?= $data['mhs']['nama']?></h5>
+    <p class="card-text">Umur : <?= $data['mhs']['umur']?></p>
+    <p class="card-text">Anime : <?= $data['mhs']['anime']?></p>
+    <p class="card-text">Status : <?= $data['mhs']['status']?></p>
     <a href="<?= BASEURL?>/mahasiswa" class="card-link">Kembali</a>
   </div>
 </div>
