4991df20e39384091ca224bf22050fd7848dee63
Jangebe <jangebe91@gmail.com>
2022-03-12 15:57:34 +0700

    coba

--- app/controllers/Waifu.php
+++ app/controllers/Waifu.php
@@ -2,14 +2,14 @@
 class Waifu extends Controller{
     public function index(){
         $data['judul'] = 'Data Waifu';
-        $data['mhs'] = $this->model('Mahasiswa_model')->getAllWaifu();
+        $data['mhs'] = $this->model('Waifu_model')->getAllWaifu();
         $this->view('templates/header', $data);
         $this->view('waifu/index', $data);
         $this->view('templates/footer');
     }
     public function detail($id){
         $data['judul'] = 'Detail Waifu';
-        $data['mhs'] = $this->model('Mahasiswa_model')->getWaifuById($id);
+        $data['mhs'] = $this->model('Waifu_model')->getWaifuById($id);
         $this->view('templates/header', $data);
         $this->view('waifu/detail', $data);
         $this->view('templates/footer');
@@ -21,7 +21,7 @@ class Waifu extends Controller{
         $this->view('templates/footer');
     }
     public function tambah(){
-        if($this->model('Mahasiswa_model')->tambahDataWaifu($_POST)>0){
+        if($this->model('Waifu_model')->tambahDataWaifu($_POST)>0){
             header ('location: '. BASEURL . '/waifu');
             exit;
         }
