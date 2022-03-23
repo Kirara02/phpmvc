93e1b8bd11a4bf8ff12262a8abdbb266280536de
Kirara02 <fathul.sirnaraja92@gmail.com>
2022-03-23 16:16:56 +0700

    getubah

--- public/js/script.js
+++ public/js/script.js
@@ -2,5 +2,20 @@ $(function(){
     $('.tampilModalUbah').on('click', function(){
         $('#formModalLabel').html('Edit Data Waifu');
         $('.modal-footer button[type=submit]').html('Ubah Data');
+
+        const id = $(this).data('id');
+
+        $.ajax({
+            url : 'http://localhost/phpmvc/public/waifu/getUbah',
+             data : {id : id},
+             method : 'post',
+             dataType : 'json',
+             success : function(data){
+                $('#anime').val(data.anime);
+                $('#nama').val(data.nama);
+                $('#umur').val(data.umur);
+                $('#status').val(data.status);
+             }
+        });
     });
 });
