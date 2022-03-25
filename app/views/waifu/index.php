cceb9006cd8c90f8455f16b83a4bfb6063fd93b9
Kirara02 <fathul.sirnaraja92@gmail.com>
2022-03-25 16:46:28 +0700

    fix modal

--- app/views/waifu/index.php
+++ app/views/waifu/index.php
@@ -14,20 +14,23 @@
         </form>
     </div>
 </div>
+<div class="row mb-3">
+    <div class="col-6">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+            Tambah Data
+        </button>
+    </div>
+</div>
 <div class="row">
         <div class="col-6"> 
             <h3>Daftar Waifu</h3>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                <button type="button" class="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#formModal">
-                Tambah Data Waifu
-                </button>
-            </div>
             <ul class="list-group">
                 <?php foreach($data['mhs'] as $mhs) : ?>
                 <li class="list-group-item">
                     <?= $mhs['nama'] ?>
                     <a href="<?= BASEURL ?>/waifu/detail/<?= $mhs['id']?>" class="badge bg-primary float-end mx-2">detail</a>
-                    <a href="<?= BASEURL ?>/waifu/edit/<?= $mhs["id"] ?>" class="badge bg-success float-end mx-2 tampilModalUbah" data-bs-toggle="modal" data-bs-target="#formModal" data-id="<?= $mhs["id"] ?>">Edit</a>           
+                    <a href="<?= BASEURL ?>/waifu/edit/<?= $mhs["id"] ?>" class="badge bg-success float-end mx-2 tampilModalUbah" id="tampilModalTambah" 
+                    data-bs-toggle="modal" data-bs-target="#formModal" data-id="<?= $mhs["id"] ?>">Edit</a>           
                     <a href="<?= BASEURL ?>/waifu/hapus/<?= $mhs['id']?>" class="badge bg-danger float-end mx-2" 
                     onclick="return confirm('yakin?')">hapus</a>
                 </li>
@@ -37,38 +40,37 @@
 </div>
 
 <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title formModalLabel" id="formModalLabel">Tambah data Waifu</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="formModalLabel">Tambah Data</h5>
+        <button type="button" class="btn-close close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL ?>/waifu/tambah" method="post">
+            <input type="hidden" name="id" id="id">
+            <div class="form-group">
+                <label for="anime">Anime</label>
+                <input type="text" name="anime" id="anime" class="form-control">
             </div>
-            <div class="modal-body">
-            <form action="<?= BASEURL ?>/waifu/tambah" method="post">
-                <input type="hidden" name="id" id="id">
-                <div class="form-group">
-                    <label for="anime">Anime</label>
-                    <input type="text" name="anime" id="anime" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="nama">Nama</label>
-                    <input type="text" name="nama" id="nama" class="form-control">
-                </div>
-                <div class="form-group w-50">
-                    <label for="umur">Umur</label>
-                    <input type="number" name="umur" id="umur" class="form-control">
-                </div>
-                <div class="form-group w-50">
-                    <label for="status">Status</label>
-                    <input type="text" name="status" id="status" class="form-control">
-                </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Tambah data</button>
-                </div>  
-            </form>
-        </div>
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" name="nama" id="nama" class="form-control">
+            </div>
+            <div class="form-group w-50">
+                <label for="umur">Umur</label>
+                <input type="number" name="umur" id="umur" class="form-control">
+            </div>
+            <div class="form-group w-50">
+                <label for="status">Status</label>
+                <input type="text" name="status" id="status" class="form-control">
+            </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah data</button>
+            </div>  
+        </form>
     </div>
+  </div>
 </div>
-
--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,26 @@
 $(function(){
     $('.tampilModalUbah').on('click', function(){
-        $('.formModalLabel').html('Edit Data Waifu');
+        $('.formModalLabel').html('Edit Data');
         $('.modal-footer button[type=submit]').html('Ubah Data');
         $('.modal-body form').attr('action', 'http://localhost/phpmvc/public/waifu/ubah'); 
         const id = $(this).data('id');
-
+        
         $.ajax({
             url : 'http://localhost/phpmvc/public/waifu/getUbah',
-             data : {id : id},
-             method : 'post',
-             dataType : 'json',
-             success : function(data){
+            data : {id : id},
+            method : 'post',
+            dataType : 'json',
+            success : function(data){
                 $('#anime').val(data.anime);
                 $('#nama').val(data.nama);
                 $('#umur').val(data.umur);
                 $('#status').val(data.status);
                 $('#id').val(data.id);
-                console.log(data);
-             }
+                
+            }
         });
+    });
+    $('.close').on('click', function(){
+        window.location.replace('http://localhost/phpmvc/public/waifu');
     });
 });
